4afb203bd291acf1dec9a09646a7df5cbe907c35
KlebersonCollab <KlebersonCollab@users.noreply.github.com>
2025-07-15 03:21:09 +0000

    deploy: 51c2945e768baf54861d598ff47b23d38ea6fadf

--- core/auth/permissions.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Permissions(str, Enum):
-    READ_USERS = "read_users"
-    CREATE_USERS = "create_users"
-    UPDATE_USERS = "update_users"
-    DELETE_USERS = "delete_users"
-    MANAGE_ROLES = "manage_roles"
--- tests/test_security.py
+++
@@ -1,9 +0,0 @@
-from core.security.security import get_password_hash, verify_password
-
-
-def test_password_hashing():
-    password = "testpassword"
-    hashed_password = get_password_hash(password)
-    assert isinstance(hashed_password, str)
-    assert verify_password(password, hashed_password)
-    assert not verify_password("wrongpassword", hashed_password)
